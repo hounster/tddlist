b459677f52f159b754ccf8f5435b6af7cf8fabcf
hounster <calhoun@kallahan.com>
2015-05-30 16:01:28 -0400

    testing vim as default editor

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention "to-do" lists:
         self.assertIn('To-Do', self.browser.title)
 
-# She is invited to enter a to-do item straight away.
+# She is invited to enter a to-do item immediately.
 
 # She types "Buy peacock feathers" into a text box.
 
