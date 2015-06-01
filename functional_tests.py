d6acd2b4b32e0e35955307e82d7a8254529202a7
hounster <calhoun@kallahan.com>
2015-06-01 19:05:58 -0400

    updated for sending/processing POST request

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -33,23 +34,29 @@ class NewVisitorTest(unittest.TestCase):
 	# When she hits ENTER, the page updates, and now the page lists
 	# "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(10)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.  She
         # enters "Use peacock feathers to make a fly for fly fishing".
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        # The page updates again, now both items are in her list.
+
+        # Edith wonders if the site will remember her list.  Then notices
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
-
-# The page updates again, now both items are in her list.
-
-# Edith wonders if the site will remember her list.  Then notices
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
 
 # She visits the URL - her to-do list is still there.
 
